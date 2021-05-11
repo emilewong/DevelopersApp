95dc555de1d77c967e1e314c87728c5d3abc4134
Emile Wong <emilewong@outlook.com>
2021-05-11 22:20:50 +0800

    Initial Commit

---
+++ Developers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Developers
+//
+//  Created by Emile Wong on 11/5/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Emile Wong on 11/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
